144d633e0aa60743bd596397b530764f6a8fe43a
Michael Lanius Friedrich <michaelfriedrich253@gmail.com>
2023-04-30 11:17:14 -0300

    added addProduct store function to ProductCard 🔧

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,8 +3,10 @@ import { Product } from '@prisma/client'
 import { convertPrice } from '@/lib/utils'
 import { Button } from '@mui/material'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import { useCartStore } from '@/store/CartStore'
 
 export default function ProductCard({ product }: { product: Product }) {
+    const { addProduct} = useCartStore()
     return (
         <div className={styles.card}>
             <div className={styles.image}>
@@ -20,7 +22,7 @@ export default function ProductCard({ product }: { product: Product }) {
                 $ {convertPrice(product.price)}
             </div>
             <div className={styles.actions}>
-                <Button variant="contained" color="primary">
+                <Button variant="contained" color="primary" onClick={() => addProduct(product)}>
                     <AddShoppingCartIcon fontSize='inherit' />
                     Add to cart
                 </Button>
